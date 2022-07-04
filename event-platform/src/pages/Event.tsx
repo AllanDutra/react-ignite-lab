9e1ba3d6dac726f3ab19b0748b90281fe1452122
AllanDutra <allandutrafeitosa14@gmail.com>
2022-07-04 20:31:50 -0300

    refactor: get slug by url params

--- event-platform/src/pages/Event.tsx
+++ event-platform/src/pages/Event.tsx
@@ -1,14 +1,20 @@
+import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
 export function Event() {
+
+    const { slug } = useParams<{ slug: string }>() // pega os parâmetros da url
+
     return (
         // "hack" para ocupar toda altura da tela
         <div className="flex flex-col min-h-screen">
             <Header />
             <main className="flex flex-1">
-                <Video />
+                {slug ?
+                    <Video lessonSlug={slug} /> :
+                    <div className="flex-1" />}
                 <Sidebar />
             </main>
         </div>
