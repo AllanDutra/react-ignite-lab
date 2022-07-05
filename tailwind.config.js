20476367c0ee52b62fca6e9b053a947fee65a638
AllanDutra <allandutrafeitosa14@gmail.com>
2022-07-05 19:47:45 -0300

    style: update path to blur-background

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        blur: 'url(/src/assets/blur-background.png)' // assets sempre dentro da pasta src e caminho sempre partindo de la.
+        blur: 'url(./src/assets/blur-background.png)' // assets sempre dentro da pasta src e caminho sempre partindo de la.
       },
       fontFamily: {
         sans: 'Roboto, sans-serif'
